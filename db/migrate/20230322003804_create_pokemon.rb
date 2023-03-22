a26a4399f1bf3cb20d0cf02552434ca94206109c
Reid Miller <reid.s.miller@gmail.com>
2023-03-21 18:38:37 -0600

    feat: Add pokemon table

---
+++ db/migrate/20230322003804_create_pokemon.rb
@@ -0,0 +1,15 @@
+class CreatePokemon < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :level
+      t.string :primary_type
+      t.string :secondary_type
+      t.string :temperment
+      t.boolean :bonded_to_traner
+      t.references :poketrainer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
