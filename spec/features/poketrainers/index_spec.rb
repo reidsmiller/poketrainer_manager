1445e8b3793e8b6e5a6b976a56dc917f0df9d895
Reid Miller <reid.s.miller@gmail.com>
2023-03-22 15:03:37 -0600

    test: Add tests for poketrainers index sort by most recently created

--- spec/features/poketrainers/index_spec.rb
+++ spec/features/poketrainers/index_spec.rb
@@ -2,15 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "/poketrainers", type: :feature do
   describe "as a visitor, when I visit the poketrainer index page" do
-    let!(:ash_ketchum) { Poketrainer.create!(name: "Ash Ketchum", age: 14, hometown: "Cerulean", gym_badges: 6, has_bike: false) }
-    let!(:misty) { Poketrainer.create!(name: "Misty", age: 13, hometown: "Somewhere", gym_badges: 5, has_bike: true) }
+    before(:each) do
+      @ash_ketchum = Poketrainer.create!(name: "Ash Ketchum", age: 14, hometown: "Cerulean", gym_badges: 6, has_bike: false, created_at: 2.days.ago)
+      @misty = Poketrainer.create!(name: "Misty", age: 13, hometown: "Somewhere", gym_badges: 5, has_bike: true, created_at: Time.current)
+      @brock = Poketrainer.create!(name: "Brock", age: 15, hometown: "Somewhere", gym_badges: 6, has_bike: true, created_at: 1.day.ago)
+    end
 
     it 'displayes the name of each poketrainer' do
       visit '/poketrainers'
 
+      save_and_open_page
+      expect(page).to have_content(@ash_ketchum.name)
+      expect(page).to have_content(@misty.name)
+      expect(page).to have_content(@brock.name)
+    end
+
+    it 'orders the records by most recently created' do
+      visit "/poketrainers"
+
       # save_and_open_page
-      expect(page).to have_content(ash_ketchum.name)
-      expect(page).to have_content(misty.name)
+      expect(@misty.name).to appear_before(@brock.name)
+      expect(@misty.name).to appear_before(@ash_ketchum.name)
+      expect(@brock.name).to appear_before(@ash_ketchum.name)
     end
   end
 end
