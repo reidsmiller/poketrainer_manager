824e6fa658de7b92b079215f2497c492c8c42ea4
Reid Miller <reid.s.miller@gmail.com>
2023-03-23 15:43:09 -0600

    test: Add tests for creating new poketrainer

---
+++ spec/features/poketrainers/new_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe '/poketrainers/new', type: :feature do
+  describe 'As a visitor, when I visit the /poketrainers/new page' do
+    it 'I see a form for a new parent record' do
+      visit '/poketrainers/new'
+
+      expect(page).to have_content('Create a New Poketrainer:')
+      expect(page).to have_content('Name:')
+      expect(page).to have_field('name')
+      expect(page).to have_content('Age:')
+      expect(page).to have_field('age')
+      expect(page).to have_content('Hometown:')
+      expect(page).to have_field('hometown')
+      expect(page).to have_content('Gym Badges:')
+      expect(page).to have_field('gym_badges')
+      expect(page).to have_content('Has a Bike?:')
+      expect(page).to have_field('has_bike')
+    end
+
+    it 'I can fill out a form with new Poketrainers attributes and click Create Parent, a POST request is sent, new record created, and I am redirected to Parent Index' do
+      visit '/poketrainers/new'
+
+      fill_in 'name', with: 'Ash Ketchum'
+      fill_in 'hometown', with: 'Pallet Town'
+      fill_in 'gym_badges', with: 6
+      fill_in 'has_bike', with: true
+
+      expect(page).to have_button('Create Poketrainer')
+
+      click_button 'Create Poketrainer'
+
+      expect(page).to have_current_path('/poketrainers')
+      expect(page).to have_content('Poketrainer Index Page')
+      expect(page).to have_content('Ash Ketchum')
+    end
+  end
+end
